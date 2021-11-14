a912a65c19eb4487b719bbb920a8a9906305c926
Kevin Wang <1776869601@qq.com>
2021-11-13 18:16:31 -0600

    Add test file structure
    The exact contents will follow later. This provides a structure for
    testing.

---
+++ tests/test_utilities.py
@@ -0,0 +1,16 @@
+import cytomulate 
+import pytest
+
+class TestUtilities():
+    
+    @classmethod
+    def setup_class(cls):
+        pass
+    
+    
+    def test_smooth_brownian_bridge(self):
+        pass
+    
+    
+    def test_generate_prufer_sequence(self):
+        pass
