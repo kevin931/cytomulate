3f36cb696787a331656f31bd82453e94a36bacfa
Yuqiu Yang <yyqengels@gmail.com>
2021-11-14 10:00:25 +0800

    Add files via upload

---
+++ cytomulate/forest.py
@@ -0,0 +1,7 @@
+from cell_type import CellType
+
+class Forest:
+    def __init__(self, n_cell_types):
+
+
+        self.trees = []
---
+++ cytomulate/tree.py
@@ -0,0 +1,31 @@
+# Math computation
+from numpy import random as rd
+
+# Tree and path generation
+from utilities import generate_random_tree
+from utilities import smooth_brownian_bridge
+
+
+class Tree:
+    def __init__(self, id, name, cell_type_list):
+        self.id = id
+        self.name = name
+        self.cell_types = cell_type_list
+        self.size = len(self.cell_types)
+        self.root = rd.choice(self.cell_types, 1)
+        self.edges = []
+        self.differentiation_paths = []
+
+    def construct_tree(self):
+        """
+        Construct a tree structure out of the cell types
+        :return: a nested list
+        """
+        node_ids = [x.id for x in self.cell_types]
+        self.edges = generate_random_tree(node_ids)
+
+
+
+
+    def construct_paths(self):
+        pass
