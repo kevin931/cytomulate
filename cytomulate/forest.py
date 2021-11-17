6b570bf9f15c0246dae352db3e2c3836384248fe
Yuqiu Yang <yyqengels@gmail.com>
2021-11-16 19:41:25 -0600

    Added some tree methods

--- cytomulate/forest.py
+++ cytomulate/forest.py
@@ -1,9 +1,49 @@
+# Math computation
+import numpy as np
+from numpy import random as rd
+
 from cell_type import CellType
 from tree import Tree
 
 
 class Forest:
-    def __init__(self, n_trees, n_cell_types):
+    def __init__(self, n_trees, n_cell_types, n_markers):
+        self.n_cell_types = n_cell_types
+        self.n_trees = n_trees
+        self.n_markers = n_markers
 
+        self.cell_types = [CellType(i, i, n_markers) for i in range(self.n_cell_types)]
 
         self.trees = []
+
+    def __contains__(self, tree_id):
+        pass
+
+    def __str__(self):
+        return "Forest"
+
+    def assign_cell_types(self):
+        nodes = rd.permutation(self.cell_types)
+        dividing_points = [self.n_cell_types]
+        if self.n_trees > 1:
+            temp = rd.choice(self.n_cell_types - 1, self.n_trees - 1, replace=False)
+            temp.sort()
+            dividing_points = np.concatenate((temp, dividing_points))
+        dividing_points = np.concatenate(([0], dividing_points))
+        for t in range(self.n_trees):
+            cell_type_list = nodes[(dividing_points[t]):(dividing_points[t + 1])]
+            root = rd.choice(cell_type_list, 1)
+            tree = Tree(t, t, cell_type_list, root)
+            self.trees.append(tree)
+
+    def sketch_trees(self):
+        for t in range(self.n_trees):
+            self.trees[t].sketch_tree()
+
+    def grow_trees(self):
+        for t in range(self.n_trees):
+            self.trees[t].grow_tree()
+
+    def visualize_forest(self):
+        pass
+
--- cytomulate/tree.py
+++ cytomulate/tree.py
@@ -7,25 +7,47 @@ from utilities import smooth_brownian_bridge
 
 
 class Tree:
-    def __init__(self, id, name, cell_type_list):
+    def __init__(self, id, name, cell_type_list, root):
         self.id = id
         self.name = name
         self.cell_types = cell_type_list
         self.size = len(self.cell_types)
-        self.root = rd.choice(self.cell_types, 1)
+        self.root = root
         self.edges = []
         self.differentiation_paths = []
 
-    def construct_tree(self):
+    def find_cell_type_by_id(self, id):
+        for c in self.cell_types:
+            if c.id == id:
+                return c
+
+    def sketch_tree(self):
         """
         Construct a tree structure out of the cell types
         :return: a nested list
         """
         node_ids = [x.id for x in self.cell_types]
         self.edges = generate_random_tree(node_ids)
+        # We then use breadth first search
+        doing_list = [self.root]
+        while len(doing_list) > 0:
+            parent_cell = doing_list.pop(0)
+            for e in self.edges:
+                if parent_cell.id in e:
+                    child_cell_id = set(e) - {parent_cell.id}
+                    if not (parent_cell.parent is None):
+                        child_cell_id = child_cell_id - {parent_cell.parent.id}
+                    if len(child_cell_id) > 0:
+                        child_cell = self.find_cell_type_by_id(child_cell_id)
+                        child_cell.parent = parent_cell
+                        doing_list.append(child_cell)
+                        parent_cell.children.append(child_cell)
 
 
 
 
-    def construct_paths(self):
+    def grow_tree(self):
+        pass
+
+    def visualize_tree(self):
         pass
