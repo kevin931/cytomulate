a189944d0716d3b0b528b7f8edae6042cb8c9d66
Kevin Wang <1776869601@qq.com>
2021-11-17 21:13:39 -0600

    Change implementation detail
    The parser is placed at the module level to allow for autodoc.
    
    - Kevin

--- cytomulate/__main__.py
+++ cytomulate/__main__.py
@@ -1,24 +1,47 @@
+"""The Command-Line Interface (CLI) of Cytomulate
+
+For convenience, users can access cytomulate without the need of writing a script. Consistent
+with python CLI syntax, this ``__main__.py`` can be accessed through ``python -m cytomulate``. 
+The CLI has the advantages of convenience while also supporting most functionalities of the
+packages.
+
+:Example:
+
+    Check version:
+
+    .. code-block:: bash
+
+        python -m cytomulate --version
+        
+    Get help:
+    
+    .. code-block:: bash
+
+        python -m cytomulate -h
+        
+**Advantage**:
+    * Shell-scripting friendly
+    * Easy-to-use
+    * No python knowledge required
+    
+**Drawbacks**:
+    * Not as configurable as a python program
+    * Top level functionality and integration only (i.e. No partial package usage)
+"""
+
+
 import cytomulate
 import argparse
 
-from typing import Optional, Sequence
+
+parser: "argparse.ArgumentParser" = argparse.ArgumentParser(description="cytomulate: CyTOF Simulation")
+parser.add_argument("--version", action="version", version=cytomulate.__version__)
 
 
 def main(args: argparse.Namespace):
     pass 
 
 
-class _Cli():
-    
-    def __init__(self) -> None:
-        self.parser: "argparse.ArgumentParser" = argparse.ArgumentParser(description="cytomulate: CyTOF Simulation")
-        self.parser.add_argument("--version", action="version", version=cytomulate.__version__)
-    
-    
-    def parse(self, args: Optional[Sequence[str]] = None) -> argparse.Namespace:
-        return self.parser.parse_args(args)
-
-
 if __name__ == "__main__":
-    cmdargs: argparse.Namespace = _Cli().parse()
+    cmdargs: argparse.Namespace = parser.parse_args()
     main(args = cmdargs)
