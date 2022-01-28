e4a9beb9cdcd97973ad9bed1879ee5a8ca64a87d
Yuqiu Yang <yyqengels@gmail.com>
2022-01-27 21:49:42 -0600

    first factor

--- cytomulate/cell_type.py
+++ cytomulate/cell_type.py
@@ -1,247 +1,78 @@
-# Math computation
+
 import numpy as np
-from numpy import random as rd
-
-# List manipulation
-from copy import deepcopy
-
-# Gaussian Mixtures
+from sklearn.cluster import KMeans
 from sklearn.mixture import GaussianMixture
-
-from typing import Union, Optional
 
 
 class CellType:
-    """The Cell Type Class
-
-    This class the basis for all cells in our simulation. It stores all the relavant information for
-    the cell and computes the expressions for the cell.
-    """
-    def __init__(self, id: int, name: Union[str, int], n_markers: int):
-        """The CellType Class constructor
-
-        This constructor sets up the bare-minimum of a cell.
-
-        :param id: The ID of the cell
-        :type id: int
-        :param name: The name of the cell
-        :type name: Union[str, int]
-        :param n_markers: The number of markers
-        :type n_markers: int
-        """
+    def __init__(self, label, id):
+        self.label = label
         self.id = id
-        self.name = name
-        self.n_markers = n_markers
+        self.highly_expressed_markers = None
+        self.lowly_expressed_markers = None
+        self.model_for_highly_expressed_markers = None
+        self.model_for_lowly_expressed_markers = None
+        self.observed_mean = None
+        self.observed_covariance = None
 
-        self.parent = None
-        self.children = []
-        self.marker_pattern = np.zeros(n_markers)
-        self.expression_level = np.zeros(n_markers)
-        self.variance_level = np.zeros(n_markers)
+    def fit(self, data, max_components, min_components, covariance_types):
+        col_median = np.median(data, axis=0).reshape(-1,1)
+        # We will use K-means with 2 groups to
+        # group markers into two groups
+        # expressed and unexpressed
+        kmeans = KMeans(n_clusters=2).fit(col_median)
 
-        # parent_cell_type will be a dictionary of size 1
-        # whose key is the id of the parent
-        # and whose value is the actual CellType
-        # object of the parent
-        self.parent_cell_type = {}
+        group_0_mean = np.mean(col_median[kmeans.labels_ == 0])
+        group_1_mean = np.mean(col_median[kmeans.labels_ == 1])
+        if group_1_mean > group_0_mean:
+            highly_expressed_group = 1
+            lowly_expressed_group = 0
+        else:
+            highly_expressed_group = 0
+            lowly_expressed_group = 1
 
-        # children_cell_types will be a dictionary
-        # whose keys are the ids of the children
-        # and whose values are the actual
-        # CellType objects of the children
-        self.children_cell_types = {}
+        self.highly_expressed_markers = np.where(kmeans.labels_ == highly_expressed_group)[0]
+        self.lowly_expressed_markers = np.where(kmeans.labels_ == lowly_expressed_group)[0]
 
-        # differential_paths_to_children will be
-        # a dictionary whose keys are the ids of
-        # the children and whose values are the
-        # collections of functions of
-        # generated differential paths
-        self.differential_paths_to_children = {}
+        # For unexpressed markers
+        # we fit gaussian mixture model with 2 components to each marginal
+        # as it seems reasonable to assume unexpressed markers are independent
+        # one with background noise
+        # one with lowly expressed protein
+        # The main purpose is to speed up the algorithm
+        # since the majority of the markers are un/lowly expressed
+        lowly_expressed_data = data[:, self.lowly_expressed_markers]
+        highly_expressed_data = data[:, self.highly_expressed_markers]
 
-        # We expect the markers_pattern to be a list of 1's and 0's
-        self.markers_pattern = np.zeros((1, n_markers))
-        self.expressed_markers = None
-        self.unexpressed_markers = None
-        self.true_zero_indices = {}
-        # We will use Gaussian Mixture to model or learn
-        # the expression of this CellType
-        # Consequently, there will be no need for
-        # us to store expression levels and
-        # variance levels separately
-        # We expect the class of this field
-        # to be sklearn.mixture._gaussian_mixture.GaussianMixture
-        self.model_for_expressed_markers = None
-        self.model_for_unexpressed_markers = None
-        # Gating markers will be a set of indices
-        self.gating_markers = set()
+        self.model_for_highly_expressed_markers = {}
+        self.model_for_lowly_expressed_markers = {}
 
-        self.observed_mean = np.zeros((1, n_markers))
-        self.observed_cov = np.zeros((n_markers, n_markers))
-        self.overall_mean = np.zeros((1, n_markers))
-        self.overall_cov = np.zeros((n_markers, n_markers))
-        self.background_noise_level = None
+        counter = 0
+        for m in self.lowly_expressed_markers:
+            self.model_for_lowly_expressed_markers[m] = GaussianMixture(n_components=2).fit(lowly_expressed_data[:, counter].reshape(-1, 1))
+            counter += 1
 
-    def sample_cell(self, n_samples = 1):
-        result = np.zeros((n_samples, self.n_markers))
-        result[:, self.expressed_markers], _ = self.model_for_expressed_markers["all"].sample(n_samples)
-        for m in self.unexpressed_markers:
-            result[:, [m]], _ = self.model_for_unexpressed_markers[m].sample(n_samples)
+        # Since we expect only a few markers to be highly expressed
+        # we can probably afford fitting the entire data
+        # with multivariate GMM as well as some model selection
+        smallest_bic = np.Inf
+        current_bic = 0
+        best_gm = None
+        for n_components in range(min_components, max_components + 1):
+            for cv_type in covariance_types:
+                gm = GaussianMixture(n_components=n_components,
+                                     covariance_type=cv_type).fit(highly_expressed_data)
+                current_bic = gm.bic(highly_expressed_data)
+                if current_bic < smallest_bic:
+                    smallest_bic = current_bic
+                    best_gm = gm
+
+        self.model_for_highly_expressed_markers["all"] = best_gm
+
+    def sample_cell(self, n_samples):
+        n_markers = len(self.lowly_expressed_markers) + len(self.highly_expressed_markers)
+        result = np.zeros((n_samples, n_markers))
+        result[:, self.highly_expressed_markers], _ = self.model_for_highly_expressed_markers["all"].sample(n_samples)
+        for m in self.lowly_expressed_markers:
+            result[:, [m]], _ = self.model_for_lowly_expressed_markers[m].sample(n_samples)
         return result
-
-    def inherit_markers_pattern(self, mutation_probability = 0.2, \
-                                n_additional_gating_markers = 2):
-        """Inherit markers pattern from parent
-
-        If the cell is not a root, this method inherits the marker pattern
-        from the parent cell.
-
-        :param mutation_probability: The mutation probability of markers by flipping the marker pattern, defaults to 0.2
-        :type mutation_probability: float, optional
-        :raises Exception: This is the root cell without a parent
-        :raises ValueError: Mutation probability is not between 0 and 1
-        :raises ValueError: Marker number exceeds the number of remaining markers (not counting gating markers)
-        :raises ValueError: Marker numbers are not positive
-        """
-        
-        if len(self.parent_cell_type) == 0:
-            raise Exception("This is the root.")
-        if mutation_probability > 1 or mutation_probability < 0:
-            raise ValueError("Mutation probability must be between 0 and 1.")
-        if n_additional_gating_markers > self.n_markers - len(self.gating_markers):
-            raise ValueError("Maker number exceeds the number of remaining markers.")
-        if n_additional_gating_markers < 0:
-            raise ValueError("Marker number has to be positive.")
-        
-        # Get the parent
-        parent = list(self.parent_cell_type.values())[0]
-        # simply copy the information needed
-        self.markers_pattern = deepcopy(parent.markers_pattern)
-        self.gating_markers = deepcopy(parent.gating_markers)
-
-        # fickle markers are indices of the markers that can change
-        # expression levels or even from 1 to 0
-        fickle_markers = set(range(self.n_markers)) - self.gating_markers
-        # flip markers are fickle markers that will flip from 0 to 1 and vice versa
-        flip_markers = set(rd.choice(list(fickle_markers), int(mutation_probability * len(fickle_markers)), False))
-        for m in flip_markers:
-            self.markers_pattern[0, m] = -1 * self.markers_pattern[0, m] + 1
-        new_gating_markers = set(rd.choice(list(fickle_markers), n_additional_gating_markers, False))
-        self.gating_markers = self.gating_markers.union(new_gating_markers)
-
-
-    def inherit_model(self):
-        """Inherit the model from the parent cell
-
-        If the cell is not the root, i.e. there is a parent cell, this model inherits
-        the Gaussian Mixture Model from the parent cell.
-
-        :raises Exception: This is the root cell without parent.
-        """
-        if len(self.parent_cell_type) == 0:
-            # if this is the root
-            raise Exception("This is the root.")
-        # Get the parent
-        parent = list(self.parent_cell_type.values())[0]
-        self.model_for_expressed_markers = deepcopy(parent.model_for_expressed_markers)
-
-
-    def fit_model(self, dat: Optional[np.ndarray] = None, max_n_components: int = 9, min_n_components: int = 1):
-        """Fit the Gaussian Mixture Model of the cell
-
-        For each cell, a Gaussian Mixture Model is used to act as the base for initial expression levels. 
-        This method fits a Gaussian Mixture Model with a range of components and the best model based on
-        BIC.
-
-        :param dat: The expression matrix for data-based simulation, defaults to None
-        :type dat: Optional[np.ndarray], optional
-        :param max_n_components: Maximum number of components, defaults to 9
-        :type max_n_components: int, optional
-        :param min_n_components: Minimum number of components, defaults to 1
-        :type min_n_components: int, optional
-        """
-        best_model = GaussianMixture(n_components = max_n_components, covariance_type = "diag").fit(dat)
-        best_bic = best_model.bic(dat)
-        for n_comp in range(min_n_components, max_n_components):
-            temp_model = GaussianMixture(n_components = n_comp, covariance_type = "diag").fit(dat)
-            temp_bic = temp_model.bic(dat)
-            if temp_bic < best_bic:
-                best_model = temp_model
-                best_bic = temp_bic
-        self.model_for_expressed_markers = best_model
-
-        self.overall_mean = self.model_for_expressed_markers.weights_.dot(self.model_for_expressed_markers.means_)
-        self.overall_var = None
-
-
-    def generate_from_paths(self, child_id: Union[str, int], alpha: float = 0.4, beta: float = 1) -> np.ndarray:
-        """Generates an array of differential points
-        
-        This array of differential points are used to simulation the differentiation path between
-        cell populations. These values are to be added to the original expressions to simulate the
-        final expressions.
-        
-        :param child_id: ID of the child to differentiate to
-        :type child_id:
-        :param alpha: alpha parameter in the Beta distribution, defaults to 0.4
-        :type alpha: float
-        :param beta: beta parameter in the Beta distribution, defaults to to 1
-        :type float: float
-        :return: an array of differential points
-        :rtype: np.ndarray
-        """
-        differential_paths = self.differential_paths_to_children[child_id]
-        differential_points = np.zeros((1, self.n_markers))
-        for m in range(self.n_markers):
-            path = differential_paths[m]
-            if path is not None:
-                # If there is no differential path, we keep it at 0
-                pseudo_time = rd.beta(a=alpha, b=beta, size=1)
-                differential_points[0, m] = path(pseudo_time)
-        return differential_points
-
-
-    def generate_initial_expressions(self) -> np.ndarray:
-        """ Generates the initial expressions of the cell
-        
-        This method samples from the Gaussian Mixture Model stored in
-        ``model_for_expressed_markers``. The resulting expressions are
-        a 1-dimensional array with a value for each marker.
-        
-        :return: The initial expressions of the cell
-        :rtype: np.ndarray
-        """
-        assert self.model_for_expressed_markers is not None
-        initial_expressions = np.zeros((1, self.n_markers))
-        initial_expressions = self.model_for_expressed_markers.sample(1)[0][0]
-        # counter = 0
-        # for m in range(self.n_markers):
-        #     if self.markers_pattern[0, m] != 0:
-        #         initial_expressions[0, m] = expressed[counter]
-        #         counter += 1
-        return initial_expressions
-
-
-    def generate_final_expressions(self, differentiate: bool = True, alpha: float = 0.4, beta: float = 1) -> np.ndarray:
-        """ Generates the final expression of the cell
-        
-        The final expression of the cell takes differentiation into consideration. The values are generated
-        using the initial expressions as a starting point and add the differentiation path, which is
-        in turn generated by a Brownian Bridge or a linear function connecting the cell and its child node.
-        
-        :param differentiate: A boolean variable indicating if differentiation is being considered,
-            defaults to True
-        :type differentiate: bool
-        :param alpha: alpha parameter in the Beta distribution, defaults to 0.4
-        :type alpha: float
-        :param beta: beta parameter in the Beta distribution, defaults to 1
-        :type beta: float
-        :return: An array of the final expressions
-        :rtype: np.ndarray
-        """
-        final_expressions = self.generate_initial_expressions()
-        if differentiate:
-            children_ids = list(self.children_cell_types)
-            child_id = rd.choice(children_ids, 1)[0]
-            differential_points = self.generate_from_paths(child_id, alpha, beta)
-            final_expressions += differential_points.reshape(self.n_markers, )
-        return final_expressions
--- cytomulate/cytof_data.py
+++ cytomulate/cytof_data.py
@@ -1,196 +1,51 @@
-# Math computation
+
 import numpy as np
-from numpy import random as rd
+from collections import Counter
 
-from sklearn import cluster
-
-from cytomulate.cell_type import CellType
-from cytomulate.forest import Forest
-from cytomulate.utilities import smooth_brownian_bridge
-
-from numpy.typing import ArrayLike
-from typing import Optional, Sequence
-
+from cell_type import CellType
 
 class CytofData:
-    def __init__(self, n_batches: int, n_trees: int, n_cells_per_batch: Sequence,\
-                 n_cell_types: int, n_markers: int, expression_matrix: Optional["np.ndarray"] = None, \
-                 cell_type_indicator: Optional[ArrayLike] = None):
-        """Class for CyTOF simulation data.
-        
-        This is the main entry point for CyTOF simulation and the main class used.
+    def __init__(self):
 
-        :param n_batches: The number of batches to simulate
-        :type n_batches: int
-        :param n_trees: The number of cell-differentiation trees
-        :type n_trees: int
-        :param n_cells_per_batch: The number of cells per batch
-        :type n_cells_per_batch: Sequence
-        :param n_markers: The number of markers
-        :type n_markers: int
-        :param expression_matrix: An existing expression matrix for data-based simulation, defaults to None
-        :type expression_matrix: np.ndarray, optional
-        :param cell_type_indicator: The names of cell types present or to be simulated
-        :type cell_type_indicator: ArrayLike, optional
-        """
-
-        if expression_matrix is not None:
-            self.simulation_mode = "Data"
-        else:
-            self.simulation_mode = "Model"
-
-        self.background_noise_level = None
-
-        self.expression_matrix = expression_matrix
-        self.cell_type_indicator = cell_type_indicator
+        self.background_noise_variance = None
         self.cell_types = {}
+        self.cell_types_data = {}
+        self.cell_type_labels_and_ids = {}
+        self.observed_cell_abundances = None
+        self.n_markers = None
+
+    def initialize_cell_types(self, expression_matrix, labels):
+        self.n_markers = np.shape(expression_matrix)[1]
+        self.cell_type_labels_and_ids = dict.fromkeys(np.unique(labels))
+        abundances = Counter(labels)
+
+        self.observed_cell_abundances = np.zeros(len(self.cell_type_labels))
+        id = 0
+        for c_type in self.cell_type_labels:
+            self.observed_cell_abundances[id] = abundances[c_type]/len(labels)
+            self.cell_type_labels[c_type] = id
+            self.cell_types[c_type] = CellType(label=c_type, id=id)
+
+            ind = np.where(labels == c_type)[0]
+            self.cell_types_data[c_type] = expression_matrix[ind, :]
+
+            self.cell_types[c_type].observed_mean = np.mean(self.cell_types_data[c_type], axis=0)
+            self.cell_types[c_type].observed_covariance = np.cov(self.cell_types_data[c_type], rowvar=False)
+
+            id += 1
+
+    def fit_cell_type_model(self, max_components=9,
+                            min_components=1,
+                            covariance_types=("full", "tied", "diag", "spherical")):
+        for c_type in self.cell_types:
+            self.cell_types[c_type].fit(data=self.cell_types_data[c_type],
+                                        max_components=max_components,
+                                        min_components=min_components,
+                                        covariance_types=covariance_types)
 
 
-        self.n_markers = n_markers
-        self.n_cell_types = n_cell_types
-
-        self.n_batches = n_batches
-        self.batch_effect_overall = np.zeros(self.n_batches)
-
-        self.batch_effect_by_cell_types_markers = {}
-        for b in range(self.n_batches):
-            self.batch_effect_by_cell_types_markers["batch" + str(b)] = np.zeros((self.n_cell_types, self.n_markers))
-
-        self.temporal_effect = []
-
-        self.n_cells_per_batch = n_cells_per_batch
-
-        self.n_trees = n_trees
-
-        self.cell_type_proportions = np.zeros((self.n_batches, n_cell_types))
-
-        self.cytof_data = {}
-        for b in range(self.n_batches):
-            self.cytof_data["batch" + str(b)] = {}
-            self.cytof_data["batch" + str(b)]["cell_type_indices"] = np.zeros(self.n_cells_per_batch[b])
-            self.cytof_data["batch" + str(b)]["expression_matrix"] = np.zeros((self.n_cells_per_batch[b], self.n_markers))
-
-        self.forest = None
-
-
-    def initialize_cell_types(self, **kwargs):
-        """ Initialize the cell types for the object based on simulation mode.
-        
-        If the ``simulation_mode`` is "Data" and if cell types are not provided, this function
-        performs a clustering on the data and fills in the cell type indicator to initialize
-        the cell types. If cell types are provided, or the simulation mode is "Model",
-        this function simply initializes cell type objects. The resulting cell types are stored
-        in the ``cell_types`` attribute of the object.
-        
-        :param **kwargs: Keyword-only arguments passed into ``sklearn.cluster.KMeans`` for clustering.
-        """
-        if (self.simulation_mode == "Data") and (self.cell_type_indicator is None):
-            assert self.expression_matrix is not None
-            cluster_model = cluster.KMeans(**kwargs).fit(self.expression_matrix)
-            self.n_cell_types = np.max(cluster_model.labels_) + 1
-            for id in range(self.n_cell_types):
-                self.cell_types[id] = CellType(id = id, name= id, n_markers=self.n_markers)
-                self.cell_types[id].fit_model(dat = self.expression_matrix[cluster_model.labels_ == id, :])
-
-        elif (self.simulation_mode == "Data") and (self.cell_type_indicator is not None):
-            assert self.expression_matrix is not None
-            cell_type_names = np.unique(self.cell_type_indicator)
-            self.n_cell_types = len(cell_type_names)
-            for id in range(len(cell_type_names)):
-                self.cell_types[id] = CellType(id=id, name=cell_type_names[id], n_markers=self.n_markers)
-                self.cell_types[id].fit_model(dat=self.expression_matrix[self.cell_type_indicator == cell_type_names[id], :])
-
-        else:
-            for id in range(self.n_cell_types):
-                self.cell_types[id] = CellType(id = id, name= id, n_markers=self.n_markers)
-
-
-    def get_markers_pattern_and_background_noise_level(self):
-        """
-        If the simulation_mode is Data then this function
-        uses the results of the clustering to estimate the
-        background noise level as well as the markers pattern
-        for each cell type
-        Otherwise, the user needs to provide one
-        :return:
-        """
+    def estimate_background_noise_variance(self):
         pass
 
-
-    def initialize_cell_type_models(self):
-        """
-        This function uses the markers pattern information
-        and the clustering result to fit a Gaussian Mixture
-        to each cell type
-        The results will be biased. We will adjust them once we
-        have the tree structure
-        :return:
-        """
+    def sample(self, n_samples):
         pass
-
-
-    def grow_forest(self):
-        """Generate a forest consisting of cell-differentiation trees.
-        
-        The forest uses ``n_trees``, ``n_cell_types``, and ``n_markers`` 
-        to grow a skeleton structure of the dataset.
-        """
-        self.forest = Forest(self.n_trees, self.n_cell_types, self.n_markers)
-        self.forest.assign_cell_types()
-        self.forest.sketch_trees()
-        self.forest.grow_trees()
-
-
-    def generate_batch_effect(self):
-        """Generate batch effect.
-        """
-        pass
-
-
-    def generate_temporal_effect(self):
-        """Generate temporal effect.
-        
-        This method generates a tempral effect uses a smooth brownian bridge for each batch. 
-        For each batch, all functions are stored in the ``temporal_effect`` attribute.
-        """
-        for b in range(self.n_batches):
-            temporal_function = smooth_brownian_bridge(0, rd.normal(0, 0.1), N=5, sigma2=0.1)
-            self.temporal_effect.append(temporal_function)
-
-
-    def generate_cell_type_proportions(self):
-        """Generate Cell Type Proportions.
-        
-        This method uses a Dirichlet Distribution to randomly generate cell type
-        proportions for each batch. 
-        """
-        self.cell_type_proportions = rd.dirichlet(np.ones(self.n_cell_types),\
-                                                  self.n_batches)
-
-
-    def grow_leaves(self):
-        """Grow leaf nodes from cell-differentiation trees of the forest.
-        
-        For each batch, leaves are assigned and computed to the trees. The
-        results are stored in the ``cytof_data`` dictionary of the object.
-        """
-        assert self.forest is not None
-        for b in range(self.n_batches):
-            cell_type_indices = rd.choice(self.n_cell_types, self.n_cells_per_batch[b],\
-                                                replace=True, p=self.cell_type_proportions[b,:])
-            self.cytof_data["batch" + str(b)]["cell_type_indices"] = cell_type_indices
-            expression_matrix = np.zeros((self.n_cells_per_batch[b], self.n_markers))
-            for event in range(self.n_cells_per_batch[b]):
-                cell = self.forest.find_cell_type_by_id(cell_type_indices[event])
-                child_cell = rd.choice(cell.children, 1)
-                for marker_id in range(self.n_markers):
-                    mu = cell.expression_level[marker_id]
-                    sig2 = cell.variance_level[marker_id]
-                    pseudo_time = rd.beta(a=0.4, b=1, size=1)
-
-                    mu += child_cell[1](pseudo_time)
-
-                    expression_matrix[event, marker_id] = rd.normal(loc = mu, scale = np.sqrt(sig2), size = 1)
-
-            self.cytof_data["batch" + str(b)]["expression_matrix"] = expression_matrix
-
