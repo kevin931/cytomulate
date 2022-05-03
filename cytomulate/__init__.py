a125a37c53127192e30543a642b2238119e20287
Yuqiu Yang <yyqengels@gmail.com>
2022-05-02 19:40:08 -0500

    Removed sim folder

--- cytomulate/__init__.py
+++ cytomulate/__init__.py
@@ -6,5 +6,3 @@ A package for simulating CyTOF data
 
 __version__ = "0.1.0"
 __author__ = "Yuqiu Yang, Kevin Wang, Tao Wang, Sherry Wang"
-
-from cytomulate import cytof_data_general, cell_graph_general, cell_type_general
