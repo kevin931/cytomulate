cb0f1d57dab91b2a24b1e9c0e07d5886bf48f4c9
Kevin Wang <kevinwang@smu.edu>
2022-05-18 19:00:38 -0500

    Add import statements
    ``CreationCytofData`` and ``EmulationCytofData`` are now package-level
    modules.

--- cytomulate/__init__.py
+++ cytomulate/__init__.py
@@ -6,3 +6,6 @@ A package for simulating CyTOF data
 
 __version__ = "0.1.0"
 __author__ = "Yuqiu Yang, Kevin Wang, Tao Wang, Sherry Wang"
+
+from cytomulate.creation.cytof_data import CreationCytofData
+from cytomulate.emulation.cytof_data import EmulationCytofData
