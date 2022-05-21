1121f9a752bf7e5e24aaba7a1218163390554b03
Yuqiu Yang <yyqengels@gmail.com>
2022-05-20 23:24:50 -0500

    Update test_general_cytof_data.py

--- tests/test_general_cytof_data.py
+++ tests/test_general_cytof_data.py
@@ -1,4 +1,6 @@
 import pytest
+import numpy as np
+from cytomulate.creation.cytof_data import CreationCytofData
 from cytomulate.cytof_data_general import GeneralCytofData
 from cytomulate.utilities import univariate_noise_model
 
@@ -25,25 +27,64 @@ def test_n_batches(Cytof_Data):
 
 
 def test_generate_cell_abundances(Cytof_Data):
-    assert hasattr(Cytof_Data, "generate_cell_abundances")
+    y = CreationCytofData()
+    y.initialize_cell_types()
+    Cytof_Data.cell_types = y.cell_types
+    Cytof_Data.n_markers = y.n_markers
+    Cytof_Data.generate_cell_abundances()
+    assert np.sum(list(Cytof_Data.cell_abundances[0].values())) <= 1.5
 
 
 def test_generate_overall_batch_effects(Cytof_Data):
-    assert hasattr(Cytof_Data, "generate_overall_batch_effects")
+    y = CreationCytofData()
+    y.initialize_cell_types()
+    Cytof_Data.cell_types = y.cell_types
+    Cytof_Data.n_markers = y.n_markers
+    Cytof_Data.generate_overall_batch_effects(variance=0.1)
+
+    assert (np.sum(list(Cytof_Data.overall_batch_effects.values())) == 0)
 
 
 def test_generate_local_batch_effects(Cytof_Data):
-    assert hasattr(Cytof_Data, "generate_local_batch_effects")
+    y = CreationCytofData()
+    y.initialize_cell_types()
+    Cytof_Data.cell_types = y.cell_types
+    Cytof_Data.n_markers = y.n_markers
+    Cytof_Data.cell_type_labels_to_ids = y.cell_type_labels_to_ids
+    Cytof_Data.cell_type_ids_to_labels = y.cell_type_ids_to_labels
+    Cytof_Data.generate_local_batch_effects(variance=0.1)
+    assert (np.sum(Cytof_Data.local_batch_effects[0][0]) == 0)
 
 
 def test_generate_temporal_effects(Cytof_Data):
-    assert hasattr(Cytof_Data, "generate_temporal_effects")
+    y = CreationCytofData()
+    y.initialize_cell_types()
+    Cytof_Data.cell_types = y.cell_types
+    Cytof_Data.n_markers = y.n_markers
+    Cytof_Data.cell_type_labels_to_ids = y.cell_type_labels_to_ids
+    Cytof_Data.cell_type_ids_to_labels = y.cell_type_ids_to_labels
+    Cytof_Data.generate_temporal_effects(variance=0.1)
+    assert Cytof_Data.temporal_effects[0][0](0) == 0
 
 
 def test_sample_one_batch(Cytof_Data):
-    assert hasattr(Cytof_Data, "sample_one_batch")
+    y = CreationCytofData()
+    y.initialize_cell_types()
+    Cytof_Data.cell_types = y.cell_types
+    Cytof_Data.n_markers = y.n_markers
+    Cytof_Data.cell_type_labels_to_ids = y.cell_type_labels_to_ids
+    Cytof_Data.cell_type_ids_to_labels = y.cell_type_ids_to_labels
+    temp = Cytof_Data.sample_one_batch(1)
+    assert (isinstance(temp[3], np.ndarray))
 
 
 def test_sample(Cytof_Data):
-    assert hasattr(Cytof_Data, "sample")
+    y = CreationCytofData()
+    y.initialize_cell_types()
+    Cytof_Data.cell_types = y.cell_types
+    Cytof_Data.n_markers = y.n_markers
+    Cytof_Data.cell_type_labels_to_ids = y.cell_type_labels_to_ids
+    Cytof_Data.cell_type_ids_to_labels = y.cell_type_ids_to_labels
+    temp = Cytof_Data.sample(1)
+    assert (isinstance(temp[3], dict))
 
