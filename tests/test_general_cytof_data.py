5ec9cd95eb5992f8c41178ef3b2306c9799018b4
Yuqiu Yang <yyqengels@gmail.com>
2022-05-20 23:12:34 -0500

    Update test_general_cytof_data.py

--- tests/test_general_cytof_data.py
+++ tests/test_general_cytof_data.py
@@ -1,6 +1,4 @@
 import pytest
-import networkx as nx
-import numpy as np
 from cytomulate.cytof_data_general import GeneralCytofData
 from cytomulate.utilities import univariate_noise_model
 
@@ -24,3 +22,28 @@ def test_background_noise_model(Cytof_Data):
 
 def test_n_batches(Cytof_Data):
     assert Cytof_Data.n_batches == 2
+
+
+def test_generate_cell_abundances(Cytof_Data):
+    assert hasattr(Cytof_Data, "generate_cell_abundances")
+
+
+def test_generate_overall_batch_effects(Cytof_Data):
+    assert hasattr(Cytof_Data, "generate_overall_batch_effects")
+
+
+def test_generate_local_batch_effects(Cytof_Data):
+    assert hasattr(Cytof_Data, "generate_local_batch_effects")
+
+
+def test_generate_temporal_effects(Cytof_Data):
+    assert hasattr(Cytof_Data, "generate_temporal_effects")
+
+
+def test_sample_one_batch(Cytof_Data):
+    assert hasattr(Cytof_Data, "sample_one_batch")
+
+
+def test_sample(Cytof_Data):
+    assert hasattr(Cytof_Data, "sample")
+
